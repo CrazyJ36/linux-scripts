0e133be43fbc27ef9a61a74ff6aa3443ee8a86ed
CrazyJ36 <crazy_j36@hotmail.com>
2018-03-03 21:41:21 +0000

    edits

--- textviewer.sh
+++ textviewer.sh
@@ -3,6 +3,7 @@ echo "This script shows file contexts one at a time."
 echo "Enter the number of the file to show, ctrl-c to quit:"
 select FILENAME in *;
 do
-     echo "showing $FILENAME ($REPLY):\n"
+     echo "showing $FILENAME ($REPLY):"
+     echo "\n"
      cat "$FILENAME"
 done
