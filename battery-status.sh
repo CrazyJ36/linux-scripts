1e43cc840ca1d1ac9a08dd29642a4521e4c4b777
Thomas Kennedy <crazy_j36@hotmail.com>
2018-03-12 00:16:08 +0000

    battery-status.sh edited online with Bitbucket

--- battery-status.sh
+++ battery-status.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Probably not for chroot stuff like Linux in Android
+# Change BAT1 to whichever you current systems battery name is.
 
 echo "Battery Percentage:"
 cat /sys/class/power_supply/BAT1/capacity
