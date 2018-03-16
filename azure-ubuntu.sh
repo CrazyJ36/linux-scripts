b775ed74d28478303c59681734c44c4f9eebb08d
CrazyJ36 <crazy_j36@hotmail.com>
2018-03-16 04:41:27 -0400

    add my ssh scripts

---
+++ azure-ubuntu.sh
@@ -0,0 +1,2 @@
+#!/data/data/com.termux/files/usr/bin/bash
+sshpass -p crazyj3463Tj@ ssh -o StrictHostKeyChecking=no thomas@52.179.98.127
---
+++ toshiba-anywhere.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sshpass -p 232130 ssh thomas@96.58.45.49
