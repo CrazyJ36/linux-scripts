2aaa10405855fb96e7bd353ef6a3d44c70692a0c
CrazyJ36 <crazy_j36@hotmail.com>
2018-04-23 10:30:59 -0400

    rm termux version of azure-ubuntu.sh from repo, use termux-fix-shebang

--- azure-ubuntu-termux.sh
+++
@@ -1,5 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-# the shebang line can be simply changed to termux 
-# compatible with 'termux-fix-shebang' in termux.
-# Save usuale file lacally and update it from there.
-sshpass -p crazyj3463Tj@ ssh -o StrictHostKeyChecking=no thomas@13.82.27.219
