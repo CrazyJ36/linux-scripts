9c917668e9f1f2e80012ad35a9c0aec05f407607
CrazyJ36 <crazy_j36@hotmail.com>
2018-04-27 18:16:29 -0400

    update

--- adb-remote-local.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar /home/thomas/development/android/adb-remote/adbportforward.jar server adblocation=/usr/bin/
--- azure-ubuntu.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sshpass -p crazyj3463Tj@ ssh -o StrictHostKeyChecking=no thomas@52.170.159.182
