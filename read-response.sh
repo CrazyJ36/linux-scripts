cc6aae5897e7f23fed93e904a9296aa65e9d373e
CrazyJ36 <crazy_j36@hotmail.com>
2018-04-28 22:19:59 -0400

    bash yes or no prompt example

--- read-response.sh
+++ read-response.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-yes="y"
-read response
-if [ response == "y" ]; then
+# Bash example of promting user for yes or no response to continue
+read -p "Continue? Options: [y, n] " response
+if [[ $response == "y" ]]; then
  echo "continue"
-elif [ response == "n" ]; then
+elif [[ $response == "n" ]]; then
  echo "exit"
 else
  echo "unknown response, abort"
