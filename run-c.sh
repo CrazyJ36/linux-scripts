f638b57a0d6bd540bb2857edbb77644834496bef
CrazyJ36 <crazy_j36@hotmail.com>
2018-04-29 00:52:56 -0400

    run file as argument in run-c.sh

--- run-c.sh
+++ run-c.sh
@@ -2,7 +2,8 @@
 
 # this enables compiling/running a c file temporarily then deleting the bin.
 # useful if you want to test c program without organizing by arch.
-read -p "Type filename, without extention, to run for this platform: " file
+# don't type extension when running ex: ./run-c.sh ctitle
+file=$1
 cc -o $file $file.c
 ./$file
 rm $file
