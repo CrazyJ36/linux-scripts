b60e6ccf816b263b295acaa1dcf3ef6f82902686
CrazyJ36 <crazy_j36@hotmail.com>
2018-04-28 22:50:34 -0400

    complete run-c-temp

--- run-c.sh
+++ run-c.sh
@@ -2,12 +2,8 @@
 
 # this enables compiling/running a c file temporarily then deleting the bin.
 # useful if you want to test c program without organizing by arch.
-echo "only type filename (no extention) as argument. continue? y,n: "
-wait=read
-if [wait
-
-cfile=$1.c
-filename=$1
-
-cc -o $filename $cfile
-./filename
+read -p "Type filename, without extention, to run for this platform: " file
+cc -o $file $file.c
+./$file
+rm $file
+exit
