31490bb78a7e13b017365770f793cc4ad47813d4
CrazyJ36 <crazy_j36@hotmail.com>
2018-05-09 17:14:08 -0400

    add response example, improve git-push by adding choice of continuing

---
+++ get-response.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+read -p "y or n: " x
+if [ "$x" == "y" ]; then
+  printf "Woohoo!\n"
+fi
--- git-push.sh
+++ git-push.sh
@@ -2,19 +2,29 @@
 message=$1
 function main {
 if [ "$message" == "-h" ] || [ "$message" == "--help" ] || [ "$message" == "help" ] || [ "$message" == "h" ] || [ "$message" == "?" ] || [ -z "$message" ]
-then
-help
-exit
+ then
+  help
+  exit
 else
+ pushOrNot
+fi
+}
+
+function pushOrNot {
 printf "Commiting changes in:\n"
 pwd
-git add .
 printf "with message:\n$message\n"
+read -p "Continue? (y,n)" x
+if [ "$x" == "y" ]
+then
+git add .
 git commit -m "$message"
 printf "Pushing to your remote repo.\n"
 git push
-printf "done\n"
+printf "Done\n"
 exit
+else
+printf "Cancelled\n"
 fi
 }
 
