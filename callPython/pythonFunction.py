dcd10beeb6583ac6393725d83d735ea00c256393
CrazyJ36 <crazy_j36@hotmail.com>
2018-05-28 10:26:32 -0400

    python in bash

---
+++ callPython/pythonFunction.py
@@ -0,0 +1,1 @@
+print("This text is being shown from A python file.\nYou can use any python functions.")
---
+++ callPython/use-python.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+function=$(python pythonFunction.py) # points to py file as a function
+echo $function # runs the py file
