cac4efaf3eed240ce6f0094bd89482c743ed81f2
CrazyJ36 <crazy_j36@hotmail.com>
2018-07-03 23:56:50 -0400

    start cd->ls

---
+++ git-pull-all.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+# Run in 'development' (repos) directory. Automatically changes to
+# repo directories, and 'git pull''s from there.
+
+cd android;git pull;cd ../bash;git pull;cd ../bat;git pull;cd ../c;git pull;cd ../configs;git pull;cd ../cpp;git pull;cd ../cs;git pull;cd ../docs;git pull;cd ../gitbook;git pull;cd ../html;git pull;cd ../java;git pull;cd ../js;git pull;cd ../kotlin;git pull;cd ../python;git pull;cd ../termux;git pull;cd ..
