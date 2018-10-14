e1b8519e6b05ab62a77fdd051ad1f7ca6fd4d3b6
CrazyJ36 <crazy_j36@hotmail.com>
2018-10-14 13:42:43 -0400

    fix append-number-to-files script

--- append-number-to-filenames.sh
+++ append-number-to-filenames.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+printf "For directory: "
+pwd
 
-num=(1)
+read -p "This will rename all files in this directory to: 1-name 2-name...etc!\nContinue? [y,n]: " x
+if [ "$x" == "y" ]; then
 
-ls ~/junk | while read -r file;do
+  num=(1)
 
-  mv $file $num-$file
-  num=$((num+1))
+  ls | while read -r file;do
 
-done
+    mv $file $num-$file
+    num=$((num+1))
+
+    done
+
+else
+  printf "Cancelled\n"
+fi
+exit
