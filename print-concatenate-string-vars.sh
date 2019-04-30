4afffdd8c362f07931688d7dc5b05f881117dc50
CrazyJ36 <crazy_j36@hotmail.com>
2019-04-30 07:52:13 +0100

    improve some, script to delete words from files

---
+++ print-concatenate-string-vars.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+str1="hello"
+str2="world"
+
+echo "strings are $str1, $str2!"
---
+++ use-variable-in-single-quotes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# show how to fix what happens when using single quotes in a statement.
+echo "hello world hello" > junk.txt
+file=junk.txt
+word="hello"
+
+cat junk.txt
+#normally variables can't be used in seds' single quotes
+# eg:
+## sed -i 's/$word/ /g' text
+# would not work as the '$word' variable is swallowed into a strin, not available to sed.
+
+# use double quotes around a section of regex to include variables:
+sed -i "s/$word/ /g" $file # this function help: sed -i 's/string-to-replace/replacement-string/g'
+cat $file
+rm junk.txt
+exit
