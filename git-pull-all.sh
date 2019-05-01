736578741debb1cd7d30fccef2b70e87bd079d62
CrazyJ36 <crazy_j36@hotmail.com>
2019-04-30 21:48:10 -0400

    remove rom entry from git maintainence scripts

--- git-pull-all.sh
+++ git-pull-all.sh
@@ -3,4 +3,4 @@
 # Automatically changes to repo directories,
 # and 'git pull''s from there.
 
-cd ~/development/android;git pull;cd ../bash;git pull;cd ../bat;git pull;cd ../c;git pull;cd ../configs;git pull;cd ../cpp;git pull;cd ../cs;git pull;cd ../docs;git pull;cd ../gitbook;git pull;cd ../html;git pull;cd ../java;git pull;cd ../js;git pull;cd ../kotlin;git pull;cd ../python;git pull;cd ../termux;git pull;cd ../rom;git pull
+cd ~/development/android;git pull;cd ../bash;git pull;cd ../bat;git pull;cd ../c;git pull;cd ../configs;git pull;cd ../cpp;git pull;cd ../cs;git pull;cd ../docs;git pull;cd ../gitbook;git pull;cd ../html;git pull;cd ../java;git pull;cd ../js;git pull;cd ../kotlin;git pull;cd ../python;git pull;cd ../termux;git pull;
