3ef0e7d6395f716d895e30652d16280137d6f5d9
CrazyJ36 <crazy_j36@hotmail.com>
2019-05-11 04:08:34 -0400

    del-words: explain how this is done

--- del-words.sh
+++ del-words.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# This would delete snyyhing between any perenthesis:
+# "s/[(][^)]*[)]/()/g"
+# You can run A sed test, not changing the file
+# by leaving out '-i'
+
 # usage:
 # del-words.sh word file
 word=$1
@@ -8,6 +14,7 @@ echo "Use as: $ del-word.sh word file"
 read -p "Deleting all instances of: [$word] from $file! Continue? Options: [y, n] " response
 if [[ $response == "y" ]]; then
  echo "Working.."
+# explaination: substitute/text/replacement/globally-in-file
  sed -i "s/$word//g" $file
  echo "Done."
 elif [[ $response == "n" ]]; then
