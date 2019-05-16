445e00407f84156d833180a65f22efa1436ea5ed
CrazyJ36 <crazy_j36@hotmail.com>
2019-05-16 04:29:59 -0400

    add configs repo, update git-pull/status scripts

--- git-pull-all.sh
+++ git-pull-all.sh
@@ -1,6 +1,10 @@
 #/bin/bash
 
-# Automatically changes to repo directories,
-# and 'git pull''s from there.
+# Automatically changes to my repo directories,
+# and runs 'git pull' from there.
 
-cd ~/development/android;git pull;cd ../assembly;git pull;cd ../bash;git pull;cd ../bat;git pull;cd ../c;git pull;cd ../configs;git pull;cd ../cpp;git pull;cd ../cs;git pull;cd ../docs;git pull;cd ../gitbook;git pull;cd ../html;git pull;cd ../java;git pull;cd ../js;git pull;cd ../kotlin;git pull;cd ../python;git pull;cd ../termux;git pull;
+# This runs in linux-like Operating Systems that can
+# use the '~' for 'home directory' symbol. Or on
+# Windows with Git Bash shell, Cygwin, etc...
+
+cd ~/development/android;git pull;cd ../assembly;git pull;cd ../bash-scripts;git pull;cd ../c;git pull;cd ../docs;git pull;cd ../html;git pull;cd ../java;git pull;cd ../js;git pull;cd ../kotlin;git pull;cd ../python;git pull;cd ../windows-scripts;git pull;
--- git-status-script.sh
+++ git-status-script.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# try to grep output of git status to detect when changed
-# print only changed.
-# this script shows when git pull or push is needed.
+# this script shows when git add, commit, then push is needed.
 
-(cd ~/development/android && printf "\n#ANDROID:";git status&&\
-cd ~/development/assembly && printf "\n#ASSEMBLY:";git status&&\
-cd ~/development/bash && printf "\n#BASH:";git status&&\
-cd ~/development/bat && printf "\n#BAT:";git status&&\
-cd ~/development/c && printf "\n#C:";git status&&\
-cd ~/development/cpp && printf "\n#CPP:";git status&&\
-cd ~/development/configs && printf "\n#CONFIG:";git status&&\
-cd ~/development/cs && printf "\n#CS:";git status&&\
-cd ~/development/docs && printf "\n#DOCS:";git status&&\
-cd ~/development/gitbook && printf "\n#GITBOOK:";git status&&\
-cd ~/development/html && printf "\n#HTML:";git status&&\
-cd ~/development/java && printf "\n#JAVA:";git status&&\
-cd ~/development/kotlin && printf "\n#KOTLIN:";git status&&\
-cd ~/development/python&& printf "\n#PYTHON:";git status&&\
-cd ~/development/termux && printf "\n#TERMUX:";git status)
+# try to grep output of git status to detect when changed.
+# auto push/pull from there.
+
+(cd ~/development/android && printf "\n#ANDROID: ";git status&&\
+cd ~/development/assembly && printf "\n#ASSEMBLY: ";git status&&\
+cd ~/development/bash-scripts && printf "\n#BASH-SCRIPTS: ";git status&&\
+cd ~/development/c && printf "\n#C: ";git status&&\
+cd ~/development/docs && printf "\n#DOCS: ";git status&&\
+cd ~/development/html && printf "\n#HTML: ";git status&&\
+cd ~/development/java && printf "\n#JAVA: ";git status&&\
+cd ~/development/js && printf "\n#JS: ";git status&&\
+cd ~/development/kotlin && printf "\n#KOTLIN: ";git status&&\
+cd ~/development/python&& printf "\n#PYTHON: ";git status&&\
+cd ~/development/windows-scripts && printf "#WINDOWS-SCRIPTS: ";git status)
