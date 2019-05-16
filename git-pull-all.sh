e723a7043e3757ce3f861cc3221d58698d913790
CrazyJ36 <crazy_j36@hotmail.com>
2019-05-16 04:40:34 -0400

    git actions are in alphabetical order

--- git-pull-all.sh
+++ git-pull-all.sh
@@ -7,4 +7,4 @@
 # use the '~' for 'home directory' symbol. Or on
 # Windows with Git Bash shell, Cygwin, etc...
 
-cd ~/development/android;git pull;cd ../assembly;git pull;cd ../linux-scripts;git pull;cd ../c;git pull;cd ../docs;git pull;cd ../html;git pull;cd ../java;git pull;cd ../js;git pull;cd ../kotlin;git pull;cd ../python;git pull;cd ../windows-scripts;git pull;
+cd ~/development/android;git pull;cd ../assembly;git pull;cd ../c;git pull;cd ../docs;git pull;cd ../html;git pull;cd ../java;git pull;cd ../js;git pull;cd ../kotlin;git pull;cd ../linux-scripts;git pull;cd ../python;git pull;cd ../windows-scripts;git pull;
--- git-status-script.sh
+++ git-status-script.sh
@@ -10,12 +10,12 @@
 
 (cd ~/development/android && printf "\n#ANDROID: ";git status&&\
 cd ~/development/assembly && printf "\n#ASSEMBLY: ";git status&&\
-cd ~/development/linux-scripts && printf "\n#LINUX-SCRIPTS: ";git status&&\
 cd ~/development/c && printf "\n#C: ";git status&&\
 cd ~/development/docs && printf "\n#DOCS: ";git status&&\
 cd ~/development/html && printf "\n#HTML: ";git status&&\
 cd ~/development/java && printf "\n#JAVA: ";git status&&\
 cd ~/development/js && printf "\n#JS: ";git status&&\
 cd ~/development/kotlin && printf "\n#KOTLIN: ";git status&&\
+cd ~/development/linux-scripts && printf "\n#LINUX-SCRIPTS: ";git status&&\
 cd ~/development/python&& printf "\n#PYTHON: ";git status&&\
 cd ~/development/windows-scripts && printf "#WINDOWS-SCRIPTS: ";git status)
