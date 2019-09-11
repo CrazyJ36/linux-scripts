46a40748c9ff9bd2c1f5f25e5b116251f3f1f65e
CrazyJ36 <crazy_j36@hotmail.com>
2019-09-11 04:15:59 -0400

    pi2-usb-gadget-setup.sh: add possible devices names

--- pi2-usb-gadget-setup.sh
+++ pi2-usb-gadget-setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 ip a add 192.168.7.1/24 dev enp0s29f7u3i1
+ip a add 192.168.7.1/24 dev enp0s29f7u3
 ip link set enp0s29f7u3i1 up
+ip link set enp0s29f7u3 up
+
 ssh -p 1024 pi@192.168.7.2
