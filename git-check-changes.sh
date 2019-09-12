f7acac9a332c361083da922628a9e840e3220a06
CrazyJ36 <crazy_j36@hotmail.com>
2019-09-11 21:26:45 -0400

    git-status-script: better approach

---
+++ git-check-changes.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# TODO: optional show git diff, auto add/commmit/push/
+
+# This script shows when local git changes are detected in A pretty format.
+# by changing directory to desired repo, then running 'git status'.
+
+
+# Edit all your default system Git account repos(matching repo top directory names) here:
+repos=(
+ android
+ assembly
+ c
+ docs
+ html
+ java
+ js
+ kotlin
+ linux-scripts
+ python
+ windows-scripts
+ )
+# Edit here to point to the directory where your git repos' are located in file-system:
+directory=~/development
+
+
+function git_status {
+  chosen_repo=$1
+  printf "Checking $chosen_repo..\n"
+  cd $directory/$chosen_repo
+
+  if [[ $(git status --porcelain) ]];
+  then
+    printf "\n"
+    git status -s
+    printf "\n"
+  fi
+
+}
+
+
+for i in "${repos[@]}"
+do
+  git_status $i
+done
+
+exit
+
+
--- same-command-in-multiple-dir.sh
+++ same-command-in-multiple-dir.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-# TODO: setup-script for this to be usable by anyone.
-# try to grep output of git status to detect when changed.
-# auto push/pull from there.
 
-# this script shows when git add, commit, and push is needed
-# by changing directory to desired repo, then checking status.
-
-
+# This prints all output from git status on all repos.
+# For A more pretty alternative, use 'git-status-script.sh'
 
 (cd ~/development/android && printf "\n#ANDROID: ";git status&&\
 cd ~/development/assembly && printf "\n#ASSEMBLY: ";git status&&\
@@ -19,3 +14,5 @@ cd ~/development/kotlin && printf "\n#KOTLIN: ";git status&&\
 cd ~/development/linux-scripts && printf "\n#LINUX-SCRIPTS: ";git status&&\
 cd ~/development/python&& printf "\n#PYTHON: ";git status&&\
 cd ~/development/windows-scripts && printf "\n#WINDOWS-SCRIPTS: ";git status)
+
+exit
