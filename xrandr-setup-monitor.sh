b154bae082d917d9ab04a21f77f97fb43577d8a6
CrazyJ36 <crazy_j36@hotmail.com>
2019-09-16 16:32:17 -0400

    working monitor script

--- xrandr-setup-monitor.sh
+++ xrandr-setup-monitor.sh
@@ -3,7 +3,7 @@ xrandr --newmode "720" 85.25  1368 1440 1576 1784  768 771 781 798 -hsync +vsync
 xrandr --addmode VGA1 720
 
 # May need to set with:
-xrandr --output VGA1 --mode 720
+xrandr --output VGA1 --right-of LVDS1 --mode 720
 
 # Also, if you can't change brightness on your monitor, you'll want to do:
-xrandr --output VGA1 --brightness 0.3
+xrandr --output VGA1 --brightness 0.5
