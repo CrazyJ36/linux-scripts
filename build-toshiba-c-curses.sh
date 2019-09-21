4e488894c78dd87ee03639a2f7947ef648ef1569
CrazyJ36 <crazy_j36@hotmail.com>
2019-09-21 08:44:28 -0400

    build-toshiba-c*: fix new development/c/c path

--- build-toshiba-c-curses.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This builds toshiba optimized curses code
-DEBIAN_BUILDARCH=pentium
-DEBIAN_GCCVER=7
-name=$1
-builder-cc -march=i686 -mtune=native -O3 -o $name $name.c -lncurses
-mv $name /home/thomas/development/c/executables/linux/86
