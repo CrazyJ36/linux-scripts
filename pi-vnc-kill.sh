9111eb794a01d407d4fe4cbf58da8cb21ac3036c
CrazyJ36 <crazy_j36@hotmail.com>
2019-09-23 02:04:40 -0400

    seperate pi naming

--- pi-vnc-kill.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# do whatever needed to kill running graphical processes
-# try systemctl stop vncserver-virtuald
-vncserver-virtual -kill :1
-pkill -STOP fluxbox
-killall -STOP fluxbox
-export DISPLAY=""
--- pi-vnc-load.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# add options for dimensions
-vncserver-virtual -kill :1
-vncserver-virtual :1
-export DISPLAY=":1"
-pkill -STOP fluxbox
-killall -STOP fluxbox
-fluxbox &
