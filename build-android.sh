26459dbde97c5c58f253b877a725503154e56b3f
CrazyJ36 <crazy_j36@hotmail.com>
2020-01-26 23:14:43 -0500

    build-android.sh: remove work_dir argument

--- build-android.sh
+++ build-android.sh
@@ -3,45 +3,46 @@
 # File edited to suit by CrazyJ36
 # using method from http://www.hanshq.net/command-line-android.html
 
-# Instructions: common java_home in linux: /usr/lib/jvm/.../
-# appname (second argument) should be the same as in ./java/com/crazyj36/name
+# Instructions: The common JAVA_HOME in linux is: /usr/lib/jvm/.../
+# this should of course be set.
+# appname argument should be the same as in ./java/com/crazyj36/name
 
 # adding libs like support libraries seems possible after "javac -cp ${PLATFORM}/android.jar:${PLATFORM}/android-support-v4.jar"
 # and "aapt -j ${PATH}/to/android-support-v4.jar"
 # But this caused crash in app as 'NoClassDefError' for this lib. My error, probably didn't have the lib downloaded.
 
-# Ideas: catch any problems before script gets to "success.."
+# You should set the directory where your android development keystore file is.
+# TODO: catch any problems before any command finishes.
 
 printf "This expects to be run in A directory that has Android source of: AndroidManifest.xml res/ java/\n"
-printf "\nRun this program as:\nbuild-android.sh SourceDir AppName KeyStorePass DeviceSerial\n"
+printf "\nRun this program as:\nbuild-android.sh AppName KeyStorePass DeviceSerial\n"
 
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
   printf "Wrong number of arguments.\n"
   exit
 else
 
-  workdir=$1
-  appname=$2
-  mpass=$3
-  device=$4
+  appname=$1
+  mpass=$2
+  device=$3
 
   jks_dir="$HOME/downloads/keystore.jks"
   JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
-  SDK="${HOME}/development/android/cli-build/aapt-build/sdk"
+  SDK="${HOME}/development/android/aapt-build/sdk"
   BUILD_TOOLS="${SDK}/build-tools"
   PLATFORM="${SDK}/platforms/"
   PLATFORM_TOOLS="${SDK}/platform-tools"
 
-  mkdir -p $workdir/build/gen $workdir/build/obj $workdir/build/apk
+  mkdir -p ./build/gen ./build/obj ./build/apk
   printf "\nbuild directories created...\n"
 
-  "${BUILD_TOOLS}/aapt" package -f -m -J $workdir/build/gen/ -S $workdir/res \
-  -M $workdir/AndroidManifest.xml -I "${PLATFORM}/android.jar"
+  "${BUILD_TOOLS}/aapt" package -f -m -J ./build/gen/ -S ./res \
+  -M ./AndroidManifest.xml -I "${PLATFORM}/android.jar"
   printf "aapt ok...\n"
 
   javac -source 1.7 -target 1.7 -bootclasspath "${JAVA_HOME}/jre/lib/rt.jar" \
-  -classpath "${PLATFORM}/android.jar" -Xlint:all -d $workdir/build/obj \
-  $workdir/build/gen/com/crazyj36/$appname/R.java $workdir/java/com/crazyj36/$appname/*.java
+  -classpath "${PLATFORM}/android.jar" -Xlint:all -d ./build/obj \
+  ./build/gen/com/crazyj36/$appname/R.java ./java/com/crazyj36/$appname/*.java
 
   if [ ! $? == 0 ]; then
     printf "Couldn't build java source files.\n"
@@ -49,35 +50,35 @@ else
   else
     printf "java ok...\n"
 
-    "${BUILD_TOOLS}/dx" --dex --output=$workdir/build/apk/classes.dex $workdir/build/obj/
+    "${BUILD_TOOLS}/dx" --dex --output=./build/apk/classes.dex ./build/obj/
     printf "dex done\n"
 
-    "${BUILD_TOOLS}/aapt" package -f -M $workdir/AndroidManifest.xml -S $workdir/res/ -I "${PLATFORM}/android.jar" \
-    -F $workdir/build/$appname-packaged.apk $workdir/build/apk/
+    "${BUILD_TOOLS}/aapt" package -f -M ./AndroidManifest.xml -S ./res/ -I "${PLATFORM}/android.jar" \
+    -F ./build/$appname-packaged.apk ./build/apk/
     printf "apk built...\n"
 
     "${BUILD_TOOLS}/apksigner" sign --v1-signing-enabled true --v2-signing-enabled false \
     --ks $jks_dir \
     --ks-key-alias CrazyJ36DevKey --ks-pass pass:$mpass --key-pass pass:$mpass \
-    --out $workdir/build/$appname-signed.apk $workdir/build/$appname-packaged.apk
+    --out ./build/$appname-signed.apk ./build/$appname-packaged.apk
 
     #jarsigner -keystore $HOME/development/android/devkey/keystore.jks \
-    #-storepass $mpass -keypass $mpass $workdir/build/$appname.aligned.apk CrazyJ36DevKey
+    #-storepass $mpass -keypass $mpass ./build/$appname.aligned.apk CrazyJ36DevKey
     printf "apk signed...\n"
 
-    "${BUILD_TOOLS}/zipalign" -f 4 $workdir/build/$appname-signed.apk $workdir/build/$appname.apk
+    "${BUILD_TOOLS}/zipalign" -f 4 ./build/$appname-signed.apk ./build/$appname.apk
     printf "zipaligned apk...\n"
 
     # adb -s $device uses your device serial number. You can set this to variable in ~/.profile.
-    "${PLATFORM_TOOLS}/adb" -s $device install -r $workdir/build/$appname.apk
+    "${PLATFORM_TOOLS}/adb" -s $device install -r ./build/$appname.apk
     printf "apk installed...\n"
 
     "${PLATFORM_TOOLS}/adb"  -s $device shell am start -n com.crazyj36.$appname/.MainActivity
     printf "app started on device\n"
 
     printf "cleaning build directories\n"
-    rm $workdir/build/$appname-packaged.apk $workdir/build/$appname-signed.apk
-    rm -rf $workdir/build/apk/ $workdir/build/obj/ $workdir/build/gen/
+    rm ./build/$appname-packaged.apk ./build/$appname-signed.apk
+    rm -rf ./build/apk/ ./build/obj/ ./build/gen/
 
     printf "done\n"
   fi
